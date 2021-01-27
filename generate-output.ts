f4f9e1067172cfc9265c9fb60bd6c05c6e73e514
sya-ri <sya79lua@gmail.com>
2021-01-27 17:38:07 +0900

    テンプレートからHTMlを生成できるようになった

--- generate-output.ts
+++ generate-output.ts
@@ -1,5 +1,22 @@
+import fs = require('fs')
+import yaml = require('js-yaml')
+import Handlebars = require('handlebars')
+
+const EventsYaml = 'events.yaml'
+const EventsTemplateHtml = 'events-template.html'
+const EventsHtml = 'events.html'
+
 const main = () => {
-    console.log("generate")
+    // events.yaml をロード
+    const data = yaml.load(fs.readFileSync(EventsYaml, 'utf8'))
+
+    // テンプレートをロード
+    const template = Handlebars.compile(fs.readFileSync(EventsTemplateHtml, 'utf8'))
+
+    // テンプレートに値を当てはめて保存
+    fs.writeFileSync(EventsHtml, template({
+        list: data
+    }), 'utf8')
 }
 
 main()
