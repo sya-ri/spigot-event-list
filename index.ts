ac8457f1ad77a0fb4151d9c6ecbaaa3ade6f97b6
sya-ri <sya79lua@gmail.com>
2021-01-27 16:05:55 +0900

    allclasses-noframeからイベント一覧を取得することに成功

--- index.ts
+++ index.ts
@@ -1,5 +1,30 @@
-const main = () => {
-    console.log('Hello World');
-};
+import request = require('request')
+import cheerio = require('cheerio')
 
-main();
+const JavaDocUrl = 'https://papermc.io/javadocs/paper/1.16/'
+const AllClassesNoFrame = 'allclasses-noframe.html'
+
+const main = () => {
+    request(JavaDocUrl + AllClassesNoFrame, function (e, response, body) {
+        if (e) {
+            console.error(e)
+        }
+
+        try {
+            const $ = cheerio.load(body)
+            const eventLinks = []
+            $('a').each(function (_, element) {
+                const a = $(element)
+                const href = a.prop("href")
+                if (href.endsWith('Event.html')) {
+                    eventLinks.push(href)
+                }
+            })
+            console.log(eventLinks)
+        } catch (e) {
+            console.error(e)
+        }
+    })
+}
+
+main()
