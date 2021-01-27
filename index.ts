ca20585abe1903f088ef349a11ffcafa10bfb25f
sya-ri <sya79lua@gmail.com>
2021-01-27 16:58:13 +0900

    events.yaml に出力する処理を追加

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 import request = require('request')
 import cheerio = require('cheerio')
+import fs = require('fs')
+import yaml = require('js-yaml')
 
 const JavaDocUrl = 'https://papermc.io/javadocs/paper/1.16/'
 const AllClassesNoFrame = 'allclasses-noframe.html'
+const EventsYaml = 'events.yaml'
 
 const main = () => {
     request(JavaDocUrl + AllClassesNoFrame, function (e, response, body) {
@@ -11,6 +14,7 @@ const main = () => {
         }
 
         try {
+            // javadoc から イベント一覧を作成
             const $ = cheerio.load(body)
             const eventLinks = []
             $('a').each(function (_, element) {
@@ -20,7 +24,24 @@ const main = () => {
                     eventLinks.push(href)
                 }
             })
-            console.log(eventLinks)
+
+            // events.yaml をロード
+            const data = yaml.load(fs.readFileSync(EventsYaml, 'utf8'))
+
+            // events.yaml に存在しない新しいイベントを追加
+            eventLinks.forEach((href) => {
+                if (!(href in data)) {
+                    data[href] = ''
+                }
+            })
+
+            // events.yaml に保存
+            fs.writeFile(EventsYaml, yaml.dump(data, { sortKeys: true }), 'utf8', (err) => {
+                if (err) {
+                    console.error(err.message);
+                }
+            })
+
         } catch (e) {
             console.error(e)
         }
