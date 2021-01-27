00cb60402866051dd8061e95fad0f462a80704f0
sya-ri <sya79lua@gmail.com>
2021-01-27 17:55:27 +0900

    events.html ではなく README.md にイベント一覧を出力するようにした

---
+++ README-template.md
@@ -0,0 +1,10 @@
+# Spigot Event List
+Bukkit, Spigot, Paper のイベント一覧です。
+
+https://papermc.io/javadocs/paper/1.16/ を元に作成しています。
+
+## イベント一覧
+{{#each list}}
+### {{name}}
+{{description}}
+{{/each}}
--- generate-output.ts
+++ generate-output.ts
@@ -1,17 +1,17 @@
 import fs = require('fs')
 import yaml = require('js-yaml')
 import Handlebars = require('handlebars')
-import {EventsHtml, EventsTemplateHtml, EventsYaml} from "./constants"
+import {OutputFileName, OutputTemplateFileName, EventsYaml} from "./constants"
 
 const main = () => {
     // events.yaml をロード
     const data = yaml.load(fs.readFileSync(EventsYaml, 'utf8'))
 
     // テンプレートをロード
-    const template = Handlebars.compile(fs.readFileSync(EventsTemplateHtml, 'utf8'))
+    const template = Handlebars.compile(fs.readFileSync(OutputTemplateFileName, 'utf8'))
 
     // テンプレートに値を当てはめて保存
-    fs.writeFileSync(EventsHtml, template({
+    fs.writeFileSync(OutputFileName, template({
         list: data
     }), 'utf8')
 }
