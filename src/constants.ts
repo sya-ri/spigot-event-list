a15721ff23e5024b6738835e0248d9cd14244b71
sya-ri <sya79lua@gmail.com>
2021-05-11 10:54:12 +0900

    Bump versions * Dependency @typescript-eslint/eslint-plugin 4.22.1 → 4.23.0 @typescript-eslint/parser 4.22.1 → 4.23.0 eslint 7.25.0 → 7.26.0 prettier 2.2.1 → 2.3.0
    * EventSource
    Paper v648 -> v660
    Purpur v1085 -> v1091

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,7 @@ export const EventSources: EventSource[] = [
     downloadUrl: "https://papermc.io/downloads#Paper-1.16",
     allClasses: "allclasses-noframe.html",
     deprecateList: "deprecated-list.html",
-    version: "#648",
+    version: "#660",
     downloadSources: ["bukkit", "spigot", "paper"],
   },
   {
@@ -14,7 +14,7 @@ export const EventSources: EventSource[] = [
     downloadUrl: "https://purpur.pl3x.net/downloads/#1.16.5",
     allClasses: "allclasses-index.html",
     deprecateList: "deprecated-list.html",
-    version: "#1085",
+    version: "#1091",
     downloadSources: ["purpur"],
   },
   {
