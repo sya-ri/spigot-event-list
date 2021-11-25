19ef06f4e68d7e44ffe2b0bc7a372831c5fd4f34
sya-ri <sya79lua@gmail.com>
2021-11-26 00:28:35 +0900

    react-router-dom を削除

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,12 @@
 import React, { lazy, Suspense } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import * as Paths from "../Paths";
 
 const Index = lazy(() => import("./pages/Index"));
 
 const App = (): JSX.Element => (
   <ChakraProvider>
     <Suspense fallback={<></>}>
-      <BrowserRouter>
-        <Routes>
-          <Route path={Paths.Index} element={<Index />} />
-        </Routes>
-      </BrowserRouter>
+      <Index />
     </Suspense>
   </ChakraProvider>
 );
