1d04a860743c30c0cd0b74cfe7ef3bcb0e5b4732
sya-ri <sya79lua@gmail.com>
2022-01-30 19:15:33 +0900

    Revert "perf: 無限スクロールを実装"
    This reverts commit 84310cf3

--- src/components/parts/EventList.tsx
+++ src/components/parts/EventList.tsx
@@ -1,6 +1,5 @@
-import React, { FC, memo, useState } from "react";
+import React, { FC, memo } from "react";
 import { Box, Container } from "@chakra-ui/react";
-import InfiniteScroll from "react-infinite-scroller";
 import EventType from "../../EventType";
 import EventListContent from "./EventListContent";
 
@@ -9,40 +8,29 @@ type Props = {
 };
 
 const EventList: FC<Props> = ({ events }) => {
-  const [renderEvents, setRenderEvents] = useState<EventType[]>([]);
-  const [hasMore, setHasMore] = useState(true);
-  const loadMore = (page: number) => {
-    if (events.length <= page) {
-      setHasMore(false);
-    } else {
-      setRenderEvents([...renderEvents, events[page]]);
-    }
-  };
   return (
     <Container px={[0, 2]}>
-      <InfiniteScroll loadMore={loadMore} hasMore={hasMore} pageStart={-1}>
-        {renderEvents.map(
-          ({
-            name,
-            link,
-            source,
-            description,
-            deprecate,
-            deprecateDescription,
-          }) => (
-            <Box key={name + source}>
-              <EventListContent
-                name={name}
-                link={link}
-                source={source}
-                description={description}
-                deprecate={deprecate}
-                deprecateDescription={deprecateDescription}
-              />
-            </Box>
-          )
-        )}
-      </InfiniteScroll>
+      {events.map(
+        ({
+          name,
+          link,
+          source,
+          description,
+          deprecate,
+          deprecateDescription,
+        }) => (
+          <Box key={name + source}>
+            <EventListContent
+              name={name}
+              link={link}
+              source={source}
+              description={description}
+              deprecate={deprecate}
+              deprecateDescription={deprecateDescription}
+            />
+          </Box>
+        )
+      )}
     </Container>
   );
 };
