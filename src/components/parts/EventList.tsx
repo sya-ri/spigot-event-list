6c9ef1203a3c9a4a9aa516eaa3d465ae965d5c18
sya-ri <sya79lua@gmail.com>
2022-01-30 19:44:03 +0900

    perf: 無限スクロールを実装

--- src/components/parts/EventList.tsx
+++ src/components/parts/EventList.tsx
@@ -1,5 +1,6 @@
-import React, { FC, memo } from "react";
+import React, { FC, memo, useEffect, useState } from "react";
 import { Box, Container } from "@chakra-ui/react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import EventType from "../../EventType";
 import EventListContent from "./EventListContent";
 
@@ -8,29 +9,49 @@ type Props = {
 };
 
 const EventList: FC<Props> = ({ events }) => {
+  const [renderEvents, setRenderEvents] = useState<EventType[]>(
+    events.slice(0, 10)
+  );
+  useEffect(() => {
+    window.scroll({ behavior: "auto", top: 0 });
+    setRenderEvents(events.slice(0, 10));
+  }, [events]);
   return (
     <Container px={[0, 2]}>
-      {events.map(
-        ({
-          name,
-          link,
-          source,
-          description,
-          deprecate,
-          deprecateDescription,
-        }) => (
-          <Box key={name + source}>
-            <EventListContent
-              name={name}
-              link={link}
-              source={source}
-              description={description}
-              deprecate={deprecate}
-              deprecateDescription={deprecateDescription}
-            />
-          </Box>
-        )
-      )}
+      <InfiniteScroll
+        next={() => {
+          const start = renderEvents.length - 1;
+          setRenderEvents([
+            ...renderEvents,
+            ...events.slice(start, start + 10),
+          ]);
+        }}
+        hasMore={true}
+        loader={<></>}
+        dataLength={renderEvents.length}
+      >
+        {renderEvents.map(
+          ({
+            name,
+            link,
+            source,
+            description,
+            deprecate,
+            deprecateDescription,
+          }) => (
+            <Box key={name + source}>
+              <EventListContent
+                name={name}
+                link={link}
+                source={source}
+                description={description}
+                deprecate={deprecate}
+                deprecateDescription={deprecateDescription}
+              />
+            </Box>
+          )
+        )}
+      </InfiniteScroll>
     </Container>
   );
 };
