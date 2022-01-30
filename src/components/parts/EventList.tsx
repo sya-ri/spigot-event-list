84310cf32590dfe00ddf17fb935fd95b11dd8f38
sya-ri <sya79lua@gmail.com>
2022-01-30 18:20:03 +0900

    perf: 無限スクロールを実装

--- src/components/parts/EventList.tsx
+++ src/components/parts/EventList.tsx
@@ -1,5 +1,6 @@
-import React, { FC, memo } from "react";
+import React, { FC, memo, useState } from "react";
 import { Box, Container } from "@chakra-ui/react";
+import InfiniteScroll from "react-infinite-scroller";
 import EventType from "../../EventType";
 import EventListContent from "./EventListContent";
 
@@ -8,32 +9,50 @@ type Props = {
 };
 
 const EventList: FC<Props> = ({ events }) => {
+  const chunkedEvents = chunked(events, 10);
+  const [renderEvents, setRenderEvents] = useState<EventType[]>([]);
+  const [hasMore, setHasMore] = useState(true);
+  const loadMore = (page: number) => {
+    if (chunkedEvents.length <= page) {
+      setHasMore(false);
+    } else {
+      setRenderEvents([...renderEvents, ...chunkedEvents[page]]);
+    }
+  };
   return (
     <Container px={[0, 2]}>
-      {events.map(
-        ({
-          name,
-          link,
-          source,
-          description,
-          deprecate,
-          deprecateDescription,
-        }) => (
-          <Box key={name + source}>
-            <EventListContent
-              name={name}
-              link={link}
-              source={source}
-              description={description}
-              deprecate={deprecate}
-              deprecateDescription={deprecateDescription}
-            />
-          </Box>
-        )
-      )}
+      <InfiniteScroll loadMore={loadMore} hasMore={hasMore} pageStart={-1}>
+        {renderEvents.map(
+          ({
+            name,
+            link,
+            source,
+            description,
+            deprecate,
+            deprecateDescription,
+          }) => (
+            <Box key={name + source}>
+              <EventListContent
+                name={name}
+                link={link}
+                source={source}
+                description={description}
+                deprecate={deprecate}
+                deprecateDescription={deprecateDescription}
+              />
+            </Box>
+          )
+        )}
+      </InfiniteScroll>
     </Container>
   );
 };
+
+const chunked = <T,>(arr: T[], size: number): T[][] =>
+  arr.reduce(
+    (_arr, _, i) => (i % size ? _arr : [..._arr, arr.slice(i, i + size)]),
+    [] as T[][]
+  );
 
 export default memo(
   EventList,
