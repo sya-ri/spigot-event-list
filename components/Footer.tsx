17de9348a98f896eb115d3e1a2b24d537da39fe8
sya-ri <sya79lua@gmail.com>
2022-02-05 16:13:21 +0900

    feat: カラーモード切り替えボタンを追加

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,46 +6,50 @@ import {
   IconButton,
   Link,
   Text,
+  useColorMode,
   useColorModeValue,
 } from "@chakra-ui/react";
 import React, { FC, memo } from "react";
-import { BiArrowToTop } from "react-icons/bi";
-import { FaGithub } from "react-icons/fa";
+import { BiArrowToTop, BiSun, BiMoon } from "react-icons/bi";
 
-const Footer: FC = () => (
-  <Box
-    bgColor={useColorModeValue("white", "gray.800")}
-    bottom={0}
-    boxShadow="0 -4px 6px -1px rgb(0 0 0 / 10%), 0 -2px 4px -1px rgb(0 0 0 / 6%)"
-    position="sticky"
-    py={1}
-  >
-    <Container maxW="container.md">
-      <Flex align="center" justify="space-around">
-        <Link onClick={() => window.scroll({ behavior: "smooth", top: 0 })}>
+const Footer: FC = () => {
+  const { toggleColorMode } = useColorMode();
+  return (
+    <Box
+      bgColor={useColorModeValue("white", "gray.800")}
+      bottom={0}
+      boxShadow="0 -4px 6px -1px rgb(0 0 0 / 10%), 0 -2px 4px -1px rgb(0 0 0 / 6%)"
+      position="sticky"
+      py={1}
+    >
+      <Container maxW="container.md">
+        <Flex align="center" justify="space-around">
+          <Link onClick={() => window.scroll({ behavior: "smooth", top: 0 })}>
+            <IconButton
+              aria-label="ScrollToTop"
+              bgColor={useColorModeValue("white", "gray.800")}
+              rounded="full"
+            >
+              <Icon as={BiArrowToTop} h={6} w={6} />
+            </IconButton>
+          </Link>
+          <Link isExternal href="https://github.com/sya-ri/spigot-event-list">
+            <Text fontWeight={500} textAlign="center">
+              ©︎ 2021 sya_ri
+            </Text>
+          </Link>
           <IconButton
             aria-label="ScrollToTop"
             bgColor={useColorModeValue("white", "gray.800")}
             rounded="full"
+            onClick={toggleColorMode}
           >
-            <Icon as={BiArrowToTop} h={6} w={6} />
+            <Icon as={useColorModeValue(BiSun, BiMoon)} h={6} w={6} />
           </IconButton>
-        </Link>
-        <Text fontWeight={500} textAlign="center">
-          ©︎ 2021 sya_ri
-        </Text>
-        <Link isExternal href="https://github.com/sya-ri/spigot-event-list">
-          <IconButton
-            aria-label="ScrollToTop"
-            bgColor={useColorModeValue("white", "gray.800")}
-            rounded="full"
-          >
-            <Icon as={FaGithub} h={6} w={6} />
-          </IconButton>
-        </Link>
-      </Flex>
-    </Container>
-  </Box>
-);
+        </Flex>
+      </Container>
+    </Box>
+  );
+};
 
 export default memo(Footer);
