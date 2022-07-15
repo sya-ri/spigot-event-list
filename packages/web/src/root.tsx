217c69a198a2904cc0179b80d9e092e23362863c
sya-ri <git@s7a.dev>
2022-07-15 13:36:10 +0900

    feat(web): favicon, ogp を設定

--- packages/web/src/root.tsx
+++ packages/web/src/root.tsx
@@ -5,10 +5,53 @@ import { Suspense } from "solid-js";
 
 export default function Root() {
   return (
-    <html lang="en">
+    <html lang="ja">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          href="apple-touch-icon.png"
+          rel="apple-touch-icon"
+          sizes="180x180"
+        />
+        <link
+          href="favicon-32x32.png"
+          rel="icon"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          href="favicon-16x16.png"
+          rel="icon"
+          sizes="16x16"
+          type="image/png"
+        />
+        <link href="site.webmanifest" rel="manifest" />
+        <link color="#606060" href="safari-pinned-tab.svg" rel="mask-icon" />
+        <meta content="#ffffff" name="msapplication-TileColor" />
+        <meta content="#ffffff" name="theme-color" />
+
+        {/* Open Graph https://www.opengraph.xyz/ */}
+        <meta content="https://spigot-event-list-solidjs.vercel.app/" property="og:url" />
+        <meta content="website" property="og:type" />
+        <meta content="Spigot Event List" property="og:title" />
+        <meta
+          content="[Minecraft 1.19] Bukkit, Spigot, Paper, Purpur, BungeeCord, Waterfall, Velocity 対応のイベント一覧"
+          property="og:title"
+        />
+        <meta
+          content="https://spigot-event-list-solidjs.vercel.app//logo.png"
+          property="og:image"
+        />
+        <meta content="1200" property="og:image:width" />
+        <meta content="630" property="og:image:height" />
+        <meta content="summary_large_image" name="twitter:card" />
+        <meta content="@sya_ri_dayo" name="twitter:site" />
+        <meta content="Spigot Event List" name="twitter:title" />
+        <meta
+          content="https://spigot-event-list-solidjs.vercel.app//logo.png"
+          name="twitter:image"
+        />
         <Meta />
         <Links />
       </head>
