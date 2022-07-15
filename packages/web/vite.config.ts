ff809ce529478e0eb5b4870b9fdb491e3996e300
sya-ri <git@s7a.dev>
2022-07-15 13:08:24 +0900

    chore(web): Vercel へのデプロイに対応

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import solid from "solid-start";
+import vercel from "solid-start-vercel";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid({ adapter: vercel() })],
 });
