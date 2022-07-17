6aeb253a0548985235cfdd175b7d7f45303e2cfe
sya-ri <git@s7a.dev>
2022-07-17 14:23:52 +0900

    refactor(web): メソッド名を変更

--- packages/web/src/routes/index.tsx
+++ packages/web/src/routes/index.tsx
@@ -2,7 +2,7 @@ import EventList from "~/components/events/EventList";
 import Header from "~/components/header/Header";
 import Footer from "~/components/footer/Footer";
 
-export default function Home() {
+export default function Index() {
   return (
     <>
       <Header />
