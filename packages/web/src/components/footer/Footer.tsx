365ea253d18057477b537e651096d1200259c7d1
sya-ri <git@s7a.dev>
2022-07-18 23:30:56 +0900

    tweak(web): ヘッダーやフッターが正しく表示されない問題を修正

--- packages/web/src/components/footer/Footer.tsx
+++ packages/web/src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import { isDarkMode, setDarkMode } from "~/states";
 
 export default function Footer() {
   return (
-    <footer class="sticky top-[100vh] z-10 py-1 bg-white dark:bg-gray-800 shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1),0_-2px_4px_-1px_rgba(0,0,0,0.06)]">
+    <footer class="sticky bottom-0 z-10 py-1 bg-white dark:bg-gray-800 shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1),0_-2px_4px_-1px_rgba(0,0,0,0.06)]">
       <div class="md:max-w-xl mx-auto flex justify-around items-center">
         <button
           class="bg-white hover:bg-gray-200 active:bg-gray-300 dark:bg-gray-800 dark:hover:bg-gray-700 dark:active:bg-gray-600 rounded-full p-2"
--- packages/web/src/routes/index.tsx
+++ packages/web/src/routes/index.tsx
@@ -6,7 +6,7 @@ export default function Index() {
   return (
     <>
       <Header />
-      <main>
+      <main class="overflow-auto">
         <EventList />
       </main>
       <Footer />
