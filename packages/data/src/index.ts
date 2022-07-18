11560b48a01430593b9b62d6dd27e85e4ed6d837
sya-ri <git@s7a.dev>
2022-07-18 18:14:08 +0900

    chore(data): プロトコルバージョンをバージョンに含めないようにした

--- packages/data/src/index.ts
+++ packages/data/src/index.ts
@@ -33,7 +33,7 @@ const sources: { [name: string]: EventSource } = {
             "<title>BungeeCord (.*) \\[Jenkins\\]</title>"
           );
           if (match) {
-            source.version = source.artifact.version + " - " + match.pop();
+            source.version = source.artifact.version.split("-")[0] + " - " + match.pop();
           }
         })
         .catch((reason) => console.error(reason)),
@@ -57,7 +57,7 @@ const sources: { [name: string]: EventSource } = {
         .get("https://api.papermc.io/v2/projects/paper/versions/1.19/")
         .then((response) => {
           const json = response.data;
-          source.version = source.artifact.version + " - #" + json.builds.pop();
+          source.version = source.artifact.version.split("-")[0] + " - #" + json.builds.pop();
         })
         .catch((reason) => console.error(reason)),
   },
@@ -80,7 +80,7 @@ const sources: { [name: string]: EventSource } = {
         .get("https://api.purpurmc.org/v2/purpur/1.19")
         .then((response) => {
           const json = response.data;
-          source.version = source.artifact.version + " - #" + json.builds.latest;
+          source.version = source.artifact.version.split("-")[0] + " - #" + json.builds.latest;
         })
         .catch((reason) => console.error(reason)),
   },
@@ -110,7 +110,7 @@ const sources: { [name: string]: EventSource } = {
             "<title>Spigot-RSS (.*) \\[Jenkins\\]</title>"
           );
           if (match) {
-            source.version = source.artifact.version + " - " + match.pop();
+            source.version = source.artifact.version.split("-")[0] + " - " + match.pop();
           }
         })
         .catch((reason) => console.error(reason)),
@@ -169,7 +169,7 @@ const sources: { [name: string]: EventSource } = {
         .get("https://api.papermc.io/v2/projects/waterfall/versions/1.19/")
         .then((response) => {
           const json = response.data;
-          source.version = source.artifact.version + " - #" + json.builds.pop();
+          source.version = source.artifact.version.split("-")[0] + " - #" + json.builds.pop();
         })
         .catch((reason) => console.error(reason)),
   },
