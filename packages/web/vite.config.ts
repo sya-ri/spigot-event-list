143b4844a9bd955fe25cf2f293882826addd8065
sya-ri <git@s7a.dev>
2022-10-08 19:35:47 +0900

    chore(web): Fix build error

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import solid from "solid-start";
+import solid from "solid-start/vite";
 import vercel from "solid-start-vercel";
 import node from "solid-start-node";
 import * as process from "process";
