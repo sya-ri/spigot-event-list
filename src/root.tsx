6409d1ad75bc3f15c7ade1820dd49759ffa81a32
sya-ri <git@s7a.dev>
2023-12-27 04:13:06 +0900

    feat: Clarity を追加

--- src/root.tsx
+++ src/root.tsx
@@ -84,6 +84,15 @@ export default function Root() {
             gtag('config', 'G-EJ15BC1R3Q');
         `}
         </script>
+        <script>
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "kc3em5faum");
+          `}
+        </script>
       </Head>
       <Body class="dark:text-white dark:bg-gray-800">
         <ErrorBoundary>
