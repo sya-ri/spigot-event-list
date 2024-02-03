2363892f960770fb59a67ff55413926f96eb3756
sya-ri <git@s7a.dev>
2024-02-03 12:14:48 +0900

    feat: Change icon

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -8,9 +8,25 @@ import clsx from "clsx";
 const font = M_PLUS_1p({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://spigot-event-list.s7a.dev"),
   title: "Spigot Event List",
   description:
     "Event list for Minecraft plugins (Bukkit, Spigot, Paper, Purpur, Bungee, Waterfall, Velocity)",
+  openGraph: {
+    type: "website",
+    url: "/",
+    siteName: "Spigot Event List",
+    description:
+      "Event list for Minecraft plugins (Bukkit, Spigot, Paper, Purpur, Bungee, Waterfall, Velocity)",
+    images: "/logo.png",
+  },
+  twitter: {
+    card: "summary_large_image",
+  },
+  icons: {
+    apple: "/icon-512x512.png",
+  },
+  manifest: "/manifest.json",
 };
 
 export default function Layout({
