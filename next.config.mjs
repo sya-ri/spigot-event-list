4f7075478907c7a2c634721c5965572fed6c3b38
sya-ri <git@s7a.dev>
2024-02-03 11:58:15 +0900

    feat: Support PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import pwa from "next-pwa"
 
-export default nextConfig;
+const withPWA = pwa({
+    dest: 'public',
+    disable: process.env.NODE_ENV === "development"
+})
+
+export default withPWA({
+})
