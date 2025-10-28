910e8d4601aee5f45477c990ee10b6d837e58378
sya-ri <git@s7a.dev>
2025-10-29 03:42:37 +0900

    feat: Add link to Ko-Fi button

--- src/components/ko-fi-button.tsx
+++ src/components/ko-fi-button.tsx
@@ -7,18 +7,28 @@ export const KoFiButton = () => {
   return (
     <div className="flex flex-col gap-2">
       {isOpen && (
-        <iframe
-          src="https://ko-fi.com/sya_ri/?hidefeed=true&widget=true&embed=true&preview=true"
-          className="border-none w-full p-1 bg-[#f9f9f9] rounded-xl"
-          height={575}
-          title="Support sya_ri"
-        />
+        <div className="bg-white rounded-xl">
+          <iframe
+              src="https://ko-fi.com/sya_ri/?hidefeed=true&widget=true&embed=true&preview=true"
+              className="border-none w-full p-1"
+              height={650}
+              title="Support sya_ri"
+          />
+          <div className="border-t border-t-gray-300 mx-2" />
+          <div className="text-gray-700 hover:text-gray-600 text-sm flex justify-center py-2">
+            <a
+                href="https://ko-fi.com/sya_ri"
+                target="_blank"
+                rel="noopener noreferrer"
+            >ko-fi.com/sya-ri</a>
+          </div>
+        </div>
       )}
       <div className="flex justify-end">
         <div className="btn-container">
           <a
             title="Support me on Ko-Fi"
-            className="kofi-button rounded-full sm:!hidden"
+            className="kofi-button sm:!hidden"
             href="https://ko-fi.com/sya_ri"
             target="_blank"
             rel="noopener noreferrer"
